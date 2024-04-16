ceaa52dbab4f96b2a349b9ed507c577aaff26b31
BrianChen027 <brianking80227@gmail.com>
2024-04-16 16:47:14 -0400

    modify controller to allow POST

--- initial/src/main/java/com/example/restservice/EmployeeController.java
+++ initial/src/main/java/com/example/restservice/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.example.restservice;
-
+  
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.restservice.Employee;
 import com.example.restservice.Employees;
 import com.example.restservice.EmployeeManager;
-  
+
+
 // Creating the REST controller
 @RestController
 @RequestMapping(path = "/employees")
@@ -23,4 +29,17 @@ public class EmployeeController {
         return employeeManager.getAllEmployees();
     }
 
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee)
+    {
+        employeeManager.addEmployee(employee);
+  
+        URI location = ServletUriComponentsBuilder
+                  .fromCurrentRequest()
+                  .path("/{id}")
+                  .buildAndExpand(employee.getId())
+                  .toUri();
+  
+        return ResponseEntity.created(location).build();
+    }
 }
